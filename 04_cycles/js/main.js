282dd86ad7ff18459fe653ac7562635b0e25e1e5
test name <test email>
2026-01-15 19:58:24 +0200

    cycles

---
+++ 04_cycles/js/main.js
@@ -0,0 +1,60 @@
+/*
+    while(){};
+    do{}while;
+    for(){};
+*/
+
+
+// while(умова){
+//  тіло цикла
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     let color = `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+
+//     document.write(`<p style='color:${color}'>${++i}. Hello World</p>`)
+// }
+
+// let i = 10;
+// while(i >= 0){
+//     document.writeln(i, '<br>')
+//     i--;
+// }
+
+// let result;
+// do{
+//     result = +prompt("2 + 2 = ?")
+// }while(result != 4)
+// document.writeln("Congratulation")
+
+// break; continue;
+
+// let i = 0;
+// for(; ;)
+// {
+//     i++
+//     document.writeln(i, '<br>')
+//     if(i == 10)
+//         break;
+// }
+
+// console.log('last', i);
+
+// while(true)
+// {
+//     let color = prompt("Enter color")
+//     if(color =='end')
+//         break;
+//     document.writeln(`<h1 style="background:${color};">Text</h1>`)
+// }
+
+// for(let i = 0; i<10; i++)
+// {
+//     let color = prompt("Enter color")
+//     console.log(i+1,color);
+//     if(color.length < 5)
+//         continue;
+
+//     document.writeln(`<h1 style="background:${color};">Text</h1>`)
+// }
